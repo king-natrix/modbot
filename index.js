f1afbcdfb507cd7e5be45710e9b67f4466112273
king-natrix <73055228+king-natrix@users.noreply.github.com>
2021-04-13 11:58:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const Discord = require ("discord.js");
+const client = new Discord.Client();
+const nitro = require('discordnitro')
+
+client.login(process.env.TOKEN);
+
+client.on('message', message => {
+    if (message.content === '>nitro') {  
+      message.channel.send(nitro(1));
+    }
+  });
+
+  client.on('message', message => {
+    if (message.content === '>help') {  
+      message.channel.send("```Help - Discord Free Nitro\n\n>nitro (give you a free nitro)```");
+    }
+  });
+
+var http = require ("https")
+
+http.createServer(function (req, res) {
+    res.write("Hello World");
+    res.end();
+}).listen(process.env.PORT)
